678e1fb36dc83e432a465ea3920dce1250ed722f
Johannes HEINZL <jh.heinzl@gmail.com>
2023-05-03 23:37:30 +0200

    Updates.

--- js/modules/rest.service.js
+++ js/modules/rest.service.js
@@ -1,6 +1,22 @@
-export const isLoggedIn = fetch(
+export const isLoggedIn = () => fetch(
     '/api/users/current', {
     headers: {
         "Accept": "application/json"
     }
 }).then((response) => response.status === 200);
+
+export const login = (username, password) => fetch('/api/users/_login', {
+    method: 'POST',
+    headers: {
+        "Accept": "application/json",
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+        'username': username,
+        'password': password
+    })
+});
+
+export const logout = () => fetch('/api/users/_logout', {
+    method: 'POST',
+});
--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
-import { isLoggedIn } from './modules/rest.service.js';
+import { isLoggedIn, login, logout } from './modules/rest.service.js';
 import { $$, hide, show } from './modules/utils.js';
 
-isLoggedIn
-.then((isLoggedIn) => {isLoggedIn ? hide($$("loginComponent")) : show($$("loginComponent"))})
-.catch((error) => {show($$("loginComponent"))});
+const checkIsLoggedIn = () => {
+    isLoggedIn()
+        .then((isLoggedIn) => { isLoggedIn ? hide($$("loginComponent")) && show($$("logoutComponent")) : show($$("loginComponent")) && hide($$("logoutComponent"))})
+        .catch((error) => { show($$("loginComponent")) && hide($$("logoutComponent")) });
+};
 
-$$("loginForm").addEventListener("submit", function(event) {
+checkIsLoggedIn();
+
+$$("loginForm").addEventListener("submit", (event) => {
     event.preventDefault();
 
-    fetch('/api/users/_login', {method: 'POST',
-    headers:{
-        "Accept": "application/json",
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },    
-    body: new URLSearchParams({
-        'username': $$("usernameInput")?.value,
-        'password': $$("passwordInput")?.value
-    })})
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-    
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    console.log(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-    
-  });
+    const username = $$("usernameInput")?.value;
+    const password = $$("passwordInput")?.value;
+
+    login(username, password)
+        .then((response) => checkIsLoggedIn())
+});
+
+$$("logoutButton").addEventListener("click", () => logout() && show($$("loginComponent")) && hide($$("logoutComponent")));
